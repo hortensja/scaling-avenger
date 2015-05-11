dc827f6308e9255377d56717294a893a381f2332
hortensja <hortensja.p@gmail.com>
2015-05-11 21:24:31 +0200

    final layout, 2 slidebars, (only) 2nd window to do

--- PROJEKT CLI/Form1.h
+++ PROJEKT CLI/Form1.h
@@ -39,7 +39,8 @@ namespace WindowsFormApplication1 {
 	private: System::Windows::Forms::RadioButton^  nocnaButton;
 	private: System::Windows::Forms::RadioButton^  dziennaButton;
 	private: System::Windows::Forms::GroupBox^  dataBox;
-	private: System::Windows::Forms::HScrollBar^  dystansScroll;
+	private: System::Windows::Forms::HScrollBar^  dystansScroll1;
+
 
 
 
@@ -52,7 +53,8 @@ namespace WindowsFormApplication1 {
 	private: System::Windows::Forms::RadioButton^  rowerowaButton;
 	private: System::Windows::Forms::RadioButton^  pieszaButton;
 	private: System::Windows::Forms::GroupBox^  dystansBox;
-	private: System::Windows::Forms::Label^  dystansLabel;
+	private: System::Windows::Forms::Label^  dystansLabel1;
+
 	private: System::Windows::Forms::Label^  dataLabel2;
 	private: System::Windows::Forms::Label^  dataLabel1;
 	private: System::Windows::Forms::DateTimePicker^  dateTimePicker2;
@@ -60,6 +62,16 @@ namespace WindowsFormApplication1 {
 	private: System::Windows::Forms::Button^  szukaj;
 	private: System::Windows::Forms::GroupBox^  poziomBox;
 	private: System::Windows::Forms::ComboBox^  poziomList;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  dystansLabel2;
+
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::HScrollBar^  dystansScroll2;
+	private: System::Windows::Forms::RadioButton^  caloButton;
+	private: System::Windows::Forms::GroupBox^  kolejnoscBox;
+	private: System::Windows::Forms::RadioButton^  dowolnaButton;
+	private: System::Windows::Forms::RadioButton^  obowiazkowaButton;
+
 
 
 
@@ -84,30 +96,40 @@ namespace WindowsFormApplication1 {
 			this->dataLabel1 = (gcnew System::Windows::Forms::Label());
 			this->dateTimePicker2 = (gcnew System::Windows::Forms::DateTimePicker());
 			this->dateTimePicker1 = (gcnew System::Windows::Forms::DateTimePicker());
-			this->dystansScroll = (gcnew System::Windows::Forms::HScrollBar());
+			this->dystansScroll1 = (gcnew System::Windows::Forms::HScrollBar());
 			this->srodekBox = (gcnew System::Windows::Forms::GroupBox());
 			this->mieszanaButton = (gcnew System::Windows::Forms::RadioButton());
 			this->rowerowaButton = (gcnew System::Windows::Forms::RadioButton());
 			this->pieszaButton = (gcnew System::Windows::Forms::RadioButton());
 			this->dystansBox = (gcnew System::Windows::Forms::GroupBox());
-			this->dystansLabel = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->dystansLabel2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->dystansScroll2 = (gcnew System::Windows::Forms::HScrollBar());
+			this->dystansLabel1 = (gcnew System::Windows::Forms::Label());
 			this->szukaj = (gcnew System::Windows::Forms::Button());
 			this->poziomBox = (gcnew System::Windows::Forms::GroupBox());
 			this->poziomList = (gcnew System::Windows::Forms::ComboBox());
+			this->caloButton = (gcnew System::Windows::Forms::RadioButton());
+			this->kolejnoscBox = (gcnew System::Windows::Forms::GroupBox());
+			this->obowiazkowaButton = (gcnew System::Windows::Forms::RadioButton());
+			this->dowolnaButton = (gcnew System::Windows::Forms::RadioButton());
 			this->typBox->SuspendLayout();
 			this->dataBox->SuspendLayout();
 			this->srodekBox->SuspendLayout();
 			this->dystansBox->SuspendLayout();
 			this->poziomBox->SuspendLayout();
+			this->kolejnoscBox->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// typBox
 			// 
 			this->typBox->Controls->Add(this->nocnaButton);
+			this->typBox->Controls->Add(this->caloButton);
 			this->typBox->Controls->Add(this->dziennaButton);
 			this->typBox->Location = System::Drawing::Point(55, 22);
 			this->typBox->Name = L"typBox";
-			this->typBox->Size = System::Drawing::Size(255, 44);
+			this->typBox->Size = System::Drawing::Size(301, 44);
 			this->typBox->TabIndex = 0;
 			this->typBox->TabStop = false;
 			this->typBox->Text = L"Typ";
@@ -115,7 +137,7 @@ namespace WindowsFormApplication1 {
 			// nocnaButton
 			// 
 			this->nocnaButton->AutoSize = true;
-			this->nocnaButton->Location = System::Drawing::Point(39, 18);
+			this->nocnaButton->Location = System::Drawing::Point(13, 17);
 			this->nocnaButton->Margin = System::Windows::Forms::Padding(1, 3, 1, 3);
 			this->nocnaButton->Name = L"nocnaButton";
 			this->nocnaButton->Size = System::Drawing::Size(69, 21);
@@ -127,7 +149,7 @@ namespace WindowsFormApplication1 {
 			// dziennaButton
 			// 
 			this->dziennaButton->AutoSize = true;
-			this->dziennaButton->Location = System::Drawing::Point(137, 18);
+			this->dziennaButton->Location = System::Drawing::Point(84, 18);
 			this->dziennaButton->Margin = System::Windows::Forms::Padding(1, 3, 1, 3);
 			this->dziennaButton->Name = L"dziennaButton";
 			this->dziennaButton->Size = System::Drawing::Size(80, 21);
@@ -142,11 +164,11 @@ namespace WindowsFormApplication1 {
 			this->dataBox->Controls->Add(this->dataLabel1);
 			this->dataBox->Controls->Add(this->dateTimePicker2);
 			this->dataBox->Controls->Add(this->dateTimePicker1);
-			this->dataBox->Location = System::Drawing::Point(55, 307);
+			this->dataBox->Location = System::Drawing::Point(55, 240);
 			this->dataBox->Margin = System::Windows::Forms::Padding(1, 3, 1, 3);
 			this->dataBox->Name = L"dataBox";
 			this->dataBox->Padding = System::Windows::Forms::Padding(1, 3, 1, 3);
-			this->dataBox->Size = System::Drawing::Size(319, 120);
+			this->dataBox->Size = System::Drawing::Size(620, 71);
 			this->dataBox->TabIndex = 2;
 			this->dataBox->TabStop = false;
 			this->dataBox->Text = L"Data";
@@ -155,7 +177,7 @@ namespace WindowsFormApplication1 {
 			// 
 			this->dataLabel2->AutoSize = true;
 			this->dataLabel2->BackColor = System::Drawing::Color::Transparent;
-			this->dataLabel2->Location = System::Drawing::Point(23, 93);
+			this->dataLabel2->Location = System::Drawing::Point(342, 30);
 			this->dataLabel2->Margin = System::Windows::Forms::Padding(1, 0, 1, 0);
 			this->dataLabel2->Name = L"dataLabel2";
 			this->dataLabel2->Size = System::Drawing::Size(24, 17);
@@ -166,7 +188,7 @@ namespace WindowsFormApplication1 {
 			// 
 			this->dataLabel1->AutoSize = true;
 			this->dataLabel1->BackColor = System::Drawing::Color::Transparent;
-			this->dataLabel1->Location = System::Drawing::Point(23, 42);
+			this->dataLabel1->Location = System::Drawing::Point(20, 31);
 			this->dataLabel1->Margin = System::Windows::Forms::Padding(1, 0, 1, 0);
 			this->dataLabel1->Name = L"dataLabel1";
 			this->dataLabel1->Size = System::Drawing::Size(24, 17);
@@ -175,7 +197,7 @@ namespace WindowsFormApplication1 {
 			// 
 			// dateTimePicker2
 			// 
-			this->dateTimePicker2->Location = System::Drawing::Point(103, 87);
+			this->dateTimePicker2->Location = System::Drawing::Point(422, 24);
 			this->dateTimePicker2->Margin = System::Windows::Forms::Padding(1, 3, 1, 3);
 			this->dateTimePicker2->Name = L"dateTimePicker2";
 			this->dateTimePicker2->Size = System::Drawing::Size(179, 25);
@@ -183,32 +205,32 @@ namespace WindowsFormApplication1 {
 			// 
 			// dateTimePicker1
 			// 
-			this->dateTimePicker1->Location = System::Drawing::Point(103, 35);
+			this->dateTimePicker1->Location = System::Drawing::Point(100, 24);
 			this->dateTimePicker1->Margin = System::Windows::Forms::Padding(1, 3, 1, 3);
 			this->dateTimePicker1->Name = L"dateTimePicker1";
 			this->dateTimePicker1->Size = System::Drawing::Size(179, 25);
 			this->dateTimePicker1->TabIndex = 0;
 			// 
-			// dystansScroll
+			// dystansScroll1
 			// 
-			this->dystansScroll->LargeChange = 5;
-			this->dystansScroll->Location = System::Drawing::Point(24, 27);
-			this->dystansScroll->Maximum = 200;
-			this->dystansScroll->Name = L"dystansScroll";
-			this->dystansScroll->Size = System::Drawing::Size(335, 21);
-			this->dystansScroll->TabIndex = 3;
-			this->dystansScroll->Scroll += gcnew System::Windows::Forms::ScrollEventHandler(this, &Form1::dystansScroll_Scroll);
+			this->dystansScroll1->LargeChange = 5;
+			this->dystansScroll1->Location = System::Drawing::Point(66, 31);
+			this->dystansScroll1->Maximum = 200;
+			this->dystansScroll1->Name = L"dystansScroll1";
+			this->dystansScroll1->Size = System::Drawing::Size(335, 21);
+			this->dystansScroll1->TabIndex = 3;
+			this->dystansScroll1->Scroll += gcnew System::Windows::Forms::ScrollEventHandler(this, &Form1::dystansScroll1_Scroll);
 			// 
 			// srodekBox
 			// 
 			this->srodekBox->Controls->Add(this->mieszanaButton);
 			this->srodekBox->Controls->Add(this->rowerowaButton);
 			this->srodekBox->Controls->Add(this->pieszaButton);
-			this->srodekBox->Location = System::Drawing::Point(55, 92);
+			this->srodekBox->Location = System::Drawing::Point(369, 22);
 			this->srodekBox->Margin = System::Windows::Forms::Padding(1, 3, 1, 3);
 			this->srodekBox->Name = L"srodekBox";
 			this->srodekBox->Padding = System::Windows::Forms::Padding(1, 3, 1, 3);
-			this->srodekBox->Size = System::Drawing::Size(359, 101);
+			this->srodekBox->Size = System::Drawing::Size(339, 44);
 			this->srodekBox->TabIndex = 1;
 			this->srodekBox->TabStop = false;
 			this->srodekBox->Text = L"Œrodek lokomocji";
@@ -216,7 +238,7 @@ namespace WindowsFormApplication1 {
 			// mieszanaButton
 			// 
 			this->mieszanaButton->AutoSize = true;
-			this->mieszanaButton->Location = System::Drawing::Point(25, 74);
+			this->mieszanaButton->Location = System::Drawing::Point(236, 17);
 			this->mieszanaButton->Margin = System::Windows::Forms::Padding(1, 3, 1, 3);
 			this->mieszanaButton->Name = L"mieszanaButton";
 			this->mieszanaButton->Size = System::Drawing::Size(87, 21);
@@ -228,7 +250,7 @@ namespace WindowsFormApplication1 {
 			// rowerowaButton
 			// 
 			this->rowerowaButton->AutoSize = true;
-			this->rowerowaButton->Location = System::Drawing::Point(171, 41);
+			this->rowerowaButton->Location = System::Drawing::Point(112, 18);
 			this->rowerowaButton->Margin = System::Windows::Forms::Padding(1, 3, 1, 3);
 			this->rowerowaButton->Name = L"rowerowaButton";
 			this->rowerowaButton->Size = System::Drawing::Size(98, 21);
@@ -240,7 +262,7 @@ namespace WindowsFormApplication1 {
 			// pieszaButton
 			// 
 			this->pieszaButton->AutoSize = true;
-			this->pieszaButton->Location = System::Drawing::Point(25, 41);
+			this->pieszaButton->Location = System::Drawing::Point(15, 17);
 			this->pieszaButton->Margin = System::Windows::Forms::Padding(1, 3, 1, 3);
 			this->pieszaButton->Name = L"pieszaButton";
 			this->pieszaButton->Size = System::Drawing::Size(69, 21);
@@ -251,35 +273,84 @@ namespace WindowsFormApplication1 {
 			// 
 			// dystansBox
 			// 
-			this->dystansBox->Controls->Add(this->dystansLabel);
-			this->dystansBox->Controls->Add(this->dystansScroll);
-			this->dystansBox->Location = System::Drawing::Point(55, 210);
+			this->dystansBox->Controls->Add(this->label2);
+			this->dystansBox->Controls->Add(this->dystansLabel2);
+			this->dystansBox->Controls->Add(this->label3);
+			this->dystansBox->Controls->Add(this->dystansScroll2);
+			this->dystansBox->Controls->Add(this->dystansLabel1);
+			this->dystansBox->Controls->Add(this->dystansScroll1);
+			this->dystansBox->Location = System::Drawing::Point(55, 90);
 			this->dystansBox->Margin = System::Windows::Forms::Padding(1, 3, 1, 3);
 			this->dystansBox->Name = L"dystansBox";
 			this->dystansBox->Padding = System::Windows::Forms::Padding(1, 3, 1, 3);
-			this->dystansBox->Size = System::Drawing::Size(457, 77);
+			this->dystansBox->Size = System::Drawing::Size(513, 127);
 			this->dystansBox->TabIndex = 4;
 			this->dystansBox->TabStop = false;
 			this->dystansBox->Text = L"Dystans";
 			// 
-			// dystansLabel
+			// label2
 			// 
-			this->dystansLabel->BackColor = System::Drawing::Color::White;
-			this->dystansLabel->Font = (gcnew System::Drawing::Font(L"Rod", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->Location = System::Drawing::Point(23, 82);
+			this->label2->Margin = System::Windows::Forms::Padding(1, 0, 1, 0);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(24, 17);
+			this->label2->TabIndex = 5;
+			this->label2->Text = L"do";
+			// 
+			// dystansLabel2
+			// 
+			this->dystansLabel2->BackColor = System::Drawing::Color::White;
+			this->dystansLabel2->Font = (gcnew System::Drawing::Font(L"Rod", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->dystansLabel->Location = System::Drawing::Point(376, 20);
-			this->dystansLabel->Margin = System::Windows::Forms::Padding(1, 0, 1, 0);
-			this->dystansLabel->Name = L"dystansLabel";
-			this->dystansLabel->Size = System::Drawing::Size(78, 32);
-			this->dystansLabel->TabIndex = 4;
-			this->dystansLabel->Text = L"km";
-			this->dystansLabel->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
+			this->dystansLabel2->Location = System::Drawing::Point(418, 73);
+			this->dystansLabel2->Margin = System::Windows::Forms::Padding(1, 0, 1, 0);
+			this->dystansLabel2->Name = L"dystansLabel2";
+			this->dystansLabel2->Size = System::Drawing::Size(78, 32);
+			this->dystansLabel2->TabIndex = 6;
+			this->dystansLabel2->Text = L"km";
+			this->dystansLabel2->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::Color::Transparent;
+			this->label3->Location = System::Drawing::Point(23, 31);
+			this->label3->Margin = System::Windows::Forms::Padding(1, 0, 1, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(24, 17);
+			this->label3->TabIndex = 4;
+			this->label3->Text = L"od";
+			// 
+			// dystansScroll2
+			// 
+			this->dystansScroll2->LargeChange = 5;
+			this->dystansScroll2->Location = System::Drawing::Point(66, 80);
+			this->dystansScroll2->Maximum = 200;
+			this->dystansScroll2->Name = L"dystansScroll2";
+			this->dystansScroll2->Size = System::Drawing::Size(335, 21);
+			this->dystansScroll2->TabIndex = 5;
+			this->dystansScroll2->Scroll += gcnew System::Windows::Forms::ScrollEventHandler(this, &Form1::dystansScroll2_Scroll);
+			// 
+			// dystansLabel1
+			// 
+			this->dystansLabel1->BackColor = System::Drawing::Color::White;
+			this->dystansLabel1->Font = (gcnew System::Drawing::Font(L"Rod", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->dystansLabel1->Location = System::Drawing::Point(418, 21);
+			this->dystansLabel1->Margin = System::Windows::Forms::Padding(1, 0, 1, 0);
+			this->dystansLabel1->Name = L"dystansLabel1";
+			this->dystansLabel1->Size = System::Drawing::Size(78, 32);
+			this->dystansLabel1->TabIndex = 4;
+			this->dystansLabel1->Text = L"km";
+			this->dystansLabel1->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
 			// 
 			// szukaj
 			// 
 			this->szukaj->Font = (gcnew System::Drawing::Font(L"Old English Text MT", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->szukaj->Location = System::Drawing::Point(573, 501);
+			this->szukaj->Location = System::Drawing::Point(556, 432);
 			this->szukaj->Margin = System::Windows::Forms::Padding(1, 3, 1, 3);
 			this->szukaj->Name = L"szukaj";
 			this->szukaj->Size = System::Drawing::Size(119, 40);
@@ -290,7 +361,7 @@ namespace WindowsFormApplication1 {
 			// poziomBox
 			// 
 			this->poziomBox->Controls->Add(this->poziomList);
-			this->poziomBox->Location = System::Drawing::Point(55, 449);
+			this->poziomBox->Location = System::Drawing::Point(55, 347);
 			this->poziomBox->Name = L"poziomBox";
 			this->poziomBox->Size = System::Drawing::Size(182, 75);
 			this->poziomBox->TabIndex = 6;
@@ -308,12 +379,58 @@ namespace WindowsFormApplication1 {
 			this->poziomList->TabIndex = 0;
 			this->poziomList->Text = L"wybierz poziom";
 			// 
+			// caloButton
+			// 
+			this->caloButton->AutoSize = true;
+			this->caloButton->Location = System::Drawing::Point(189, 17);
+			this->caloButton->Margin = System::Windows::Forms::Padding(1, 3, 1, 3);
+			this->caloButton->Name = L"caloButton";
+			this->caloButton->Size = System::Drawing::Size(108, 21);
+			this->caloButton->TabIndex = 2;
+			this->caloButton->TabStop = true;
+			this->caloButton->Text = L"Ca³odobowa";
+			this->caloButton->UseVisualStyleBackColor = true;
+			// 
+			// kolejnoscBox
+			// 
+			this->kolejnoscBox->Controls->Add(this->dowolnaButton);
+			this->kolejnoscBox->Controls->Add(this->obowiazkowaButton);
+			this->kolejnoscBox->Location = System::Drawing::Point(300, 347);
+			this->kolejnoscBox->Name = L"kolejnoscBox";
+			this->kolejnoscBox->Size = System::Drawing::Size(200, 75);
+			this->kolejnoscBox->TabIndex = 7;
+			this->kolejnoscBox->TabStop = false;
+			this->kolejnoscBox->Text = L"Kolejnoœæ potwierdzania PK";
+			// 
+			// obowiazkowaButton
+			// 
+			this->obowiazkowaButton->AutoSize = true;
+			this->obowiazkowaButton->Location = System::Drawing::Point(22, 25);
+			this->obowiazkowaButton->Name = L"obowiazkowaButton";
+			this->obowiazkowaButton->Size = System::Drawing::Size(118, 21);
+			this->obowiazkowaButton->TabIndex = 0;
+			this->obowiazkowaButton->TabStop = true;
+			this->obowiazkowaButton->Text = L"obowi¹zkowa";
+			this->obowiazkowaButton->UseVisualStyleBackColor = true;
+			// 
+			// dowolnaButton
+			// 
+			this->dowolnaButton->AutoSize = true;
+			this->dowolnaButton->Location = System::Drawing::Point(22, 52);
+			this->dowolnaButton->Name = L"dowolnaButton";
+			this->dowolnaButton->Size = System::Drawing::Size(84, 21);
+			this->dowolnaButton->TabIndex = 1;
+			this->dowolnaButton->TabStop = true;
+			this->dowolnaButton->Text = L"dowolna";
+			this->dowolnaButton->UseVisualStyleBackColor = true;
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 17);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::OliveDrab;
-			this->ClientSize = System::Drawing::Size(718, 552);
+			this->ClientSize = System::Drawing::Size(718, 501);
+			this->Controls->Add(this->kolejnoscBox);
 			this->Controls->Add(this->poziomBox);
 			this->Controls->Add(this->szukaj);
 			this->Controls->Add(this->srodekBox);
@@ -331,15 +448,22 @@ namespace WindowsFormApplication1 {
 			this->srodekBox->ResumeLayout(false);
 			this->srodekBox->PerformLayout();
 			this->dystansBox->ResumeLayout(false);
+			this->dystansBox->PerformLayout();
 			this->poziomBox->ResumeLayout(false);
+			this->kolejnoscBox->ResumeLayout(false);
+			this->kolejnoscBox->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 
-private: System::Void dystansScroll_Scroll(System::Object^  sender, System::Windows::Forms::ScrollEventArgs^  e) {
+private: System::Void dystansScroll1_Scroll(System::Object^  sender, System::Windows::Forms::ScrollEventArgs^  e) {
 
-	dystansLabel->Text = (dystansScroll->Value).ToString() + " km";
+	dystansLabel1->Text = (dystansScroll1->Value).ToString() + " km";
+}
+private: System::Void dystansScroll2_Scroll(System::Object^  sender, System::Windows::Forms::ScrollEventArgs^  e) {
+
+	dystansLabel2->Text = (dystansScroll2->Value).ToString() + " km";
 }
 };
 }
